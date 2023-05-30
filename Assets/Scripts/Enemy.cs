4bd8884bdf44d4ffa20422ca7e859568e1b75cf1
AdrianNYV <122915251+AdrianNYV@users.noreply.github.com>
2023-05-30 20:09:07 +0200

    First Commit with base Scenenario

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+    
+public class Enemy : MonoBehaviour {
+       
+    public Transform player;
+
+    private NavMeshAgent agent;
+       
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = player.position; 
+    }
+
+    void Update() {
+        agent.destination = player.position; 
+    }
+}
